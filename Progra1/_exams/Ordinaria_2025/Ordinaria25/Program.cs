f772eea78993ba6a3c159595d35658f708f5ad0c
Juan <juasan15@ucm.es>
2025-12-16 23:45:17 +0100

    estoy reventao
    me voya  sobar

--- Progra1/_exams/Ordinaria_2025/Ordinaria25/Program.cs
+++ Progra1/_exams/Ordinaria_2025/Ordinaria25/Program.cs
@@ -12,16 +12,59 @@ namespace Main
             int[] montones = new int[NUM_MONTONES];
             int turno;
 
+            int num = 0;
+            int mon = 0;
 
-            Inicializa(montones, jugadores, turno);
+            Inicializa(montones, jugadores, out turno);
+            Render(montones, jugadores, turno, num, mon);
         }
 
-        static void Inicializa(int[] montones, string[] jugadores, int turno)
+        static void Inicializa(int[] montones, string[] jugadores, out int turno)
         {
             int cantJugadores = jugadores.Length;
 
+            for (int i = 0; i < NUM_MONTONES; i++)
+            {
+                montones[i] = rnd.Next(1, MAX_PALILLOS + 1);
+            }
+            turno = rnd.Next(0, cantJugadores + 1);
+        }
 
-            turno = rnd.Next(0,cantJugadores + 1);
+        static void Render(int[] montones, string[] jugadores, int turno, int num, int mon)
+        {
+            if (num == 0) { Console.Write("Empieza el juego!!"); }
+            else if (num > 0)
+            {
+                Console.Write($"{jugadores[turno]} quita {num} del montón {mon}");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < montones.Length; i++)
+            {
+                Console.Write(i + ": ");
+                for (int j = 0; j < montones[i]; j++)
+                {
+                    Console.Write("|");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void JuegaHumano(int[] montones, int mon, int pals)
+        {
+            Console.Write("Humano, elige montón (-1 para terminar): ");
+            mon = int.Parse(Console.ReadLine());
+
+            if (mon == -1) return;
+            else if (mon >= 0 && mon < montones.Length)
+            {
+                do
+                {
+                    Console.Write($"Cuántos palillos quieres del montón {mon}: ");
+                    pals = int.Parse(Console.ReadLine());
+                } while (pals < 1 || pals > montones[mon]);
+
+            }
         }
     }
 }
