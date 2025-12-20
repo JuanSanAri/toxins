533bdd5e7b405473681e5edcbc327a238ee39f92
Juan <juasan15@ucm.es>
2025-12-20 21:02:42 +0100

    avance examen ordinaria 25-25
    el que suspendí si

--- Progra1/_exams/Ordinaria_2025/Ordinaria_2025/Program.cs
+++ Progra1/_exams/Ordinaria_2025/Ordinaria_2025/Program.cs
@@ -2,10 +2,12 @@
 {
     internal class Program
     {
+        const int N = 4; // tamaño del tablero de ejemplo
+        // IMPORTANTE: se debe cambir el tamaño de N para que corresponda
+        // con el tamaño de nuestra matriz cuadrada de tamaño NxN
 
         static void Main(string[] args)
         {
-            const int N = 4; // tamaño del tablero de ejemplo
             char[,] tab;
             tab = new char[N, N] { // tablero del ejemplo
                 {'.','1','.','0'}, // fila 0
@@ -16,7 +18,37 @@
 
             bool[,] fijas = new bool[N, N]; // matriz de posiciones fijas
             int fil, col; // fila y columna de la casilla activa
-            Console.WriteLine("Hello, World!");
+
+            Inicializa(tab, fijas, out fil, out col);
+            Renderiza(tab, fijas, fil, col);
+        }
+
+        static void Inicializa(char[,] tab, bool[,] fijas, out int fil, out int col)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (tab[i, j] == '.') { fijas[i, j] = false; }
+                    else { fijas[i, j] = true; }
+                }
+            }
+            fil = col = 0;
+        }
+
+        static void Renderiza(char[,] tab, bool[,] fijas, int fil, int col)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (fijas[i, j] == true) { Console.ForegroundColor = ConsoleColor.Blue; }
+                    else { Console.ForegroundColor = ConsoleColor.Yellow; }
+                    Console.Write(tab[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ResetColor();
         }
     }
 }
