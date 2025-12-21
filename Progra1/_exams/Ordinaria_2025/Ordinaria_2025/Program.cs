e6237f4b8caa3eee30d5911124fc8d2d5dd3956f
Juan <juasan15@ucm.es>
2025-12-21 04:13:47 +0100

    Programa terminado
    falta añadir leer archivo

--- Progra1/_exams/Ordinaria_2025/Ordinaria_2025/Program.cs
+++ Progra1/_exams/Ordinaria_2025/Ordinaria_2025/Program.cs
@@ -30,194 +30,227 @@ namespace Ordinaria_2025
                 ProcesaInput(c, tab, fijas, ref fil, ref col);
 
                 Renderiza(tab, fijas, fil, col);
-
-                // DEBUG
-                Console.SetCursorPosition(0, 10);
-                Console.WriteLine($"(fila: {fil}, columna: {col})");
-                for (int i = 0; i < N; i++)
-                {
-                    for (int j = 0; j < N; j++)
-                    {
-                        Console.Write(tab[i, j] + " ");
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine();
-                Console.WriteLine();
-
-                //SacaFilCol(k, tab, out filk, out colk);
             }
-            if (TabLleno(tab))
+
+            // Condiciones de finalización
+            if (TabLleno(tab) && !MuestraResultado(tab))
             {
                 Console.Clear();
-                Console.WriteLine("epa");
-            }
-
-            static void Inicializa(char[,] tab, bool[,] fijas, out int fil, out int col)
-            {
-                for (int i = 0; i < N; i++)
-                {
-                    for (int j = 0; j < N; j++)
-                    {
-                        if (tab[i, j] == '.') { fijas[i, j] = false; }
-                        else { fijas[i, j] = true; }
-                    }
-                }
-                fil = col = 0;
-                Console.SetCursorPosition(col, fil);
-            }
-
-            static void Renderiza(char[,] tab, bool[,] fijas, int fil, int col)
-            {
-                Console.Clear();
-                for (int i = 0; i < N; i++)
-                {
-                    for (int j = 0; j < N; j++)
-                    {
-                        // Color del texto
-                        if (fijas[i, j] == true) { Console.ForegroundColor = ConsoleColor.Blue; }
-                        else { Console.ForegroundColor = ConsoleColor.Yellow; }
-
-                        // Color del fondo dependiendo de la posición del jugador
-                        if (col == j && fil == i)
-                        {
-                            Console.BackgroundColor = ConsoleColor.Yellow;
-                            Console.ForegroundColor = ConsoleColor.Black;
-                            Console.Write(tab[i, j]);
-                            Console.ResetColor();
-                            Console.Write(" ");
-                        }
-                        else { Console.Write(tab[i, j] + " "); }
-                    }
-                    Console.WriteLine();
-                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Enhorabuena, lo has conseguido!!");
                 Console.ResetColor();
             }
-
-            static char LeeInput()
+            else
             {
-                char d = ' ';
-                string tecla = Console.ReadKey(true).Key.ToString();
-
-                switch (tecla)
-                {
-                    // Movimiento del cursor 	
-                    case "LeftArrow": d = 'l'; break;
-                    case "UpArrow": d = 'u'; break;
-                    case "RightArrow": d = 'r'; break;
-                    case "DownArrow": d = 'd'; break;
-
-                    // Casillas
-                    case "Spacebar": d = 'c'; break;
-                    case "D0": d = '0'; break;
-                    case "D1": d = '1'; break;
-
-                    // Terminar juego
-                    case "Escape": case "q": d = 'q'; break;
-
-                    default: d = ' '; break;
-                }
-                return d;
+                Renderiza(tab, fijas, fil, col);
+                Console.SetCursorPosition(0, 5);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Casi casi!!");
+                MuestraResultado(tab);
+                Console.ResetColor();
             }
+        }
 
-            static void ProcesaInput(char c, char[,] tab, bool[,] fijas, ref int fil, ref int col)
+        static void Inicializa(char[,] tab, bool[,] fijas, out int fil, out int col)
+        {
+            for (int i = 0; i < N; i++)
             {
-                // eje x
-                if (c == 'l' && col > 0) { col--; }
-                else if (c == 'r' && col < N - 1) { col++; }
-                // eje y
-                if (c == 'd' && fil < N - 1) { fil++; }
-                else if (c == 'u' && fil > 0) { fil--; }
-
-                // casillas
-                for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
                 {
-                    for (int j = 0; j < N; j++)
-                    {
-                        if (fijas[i, j] == false && col == j && fil == i)
-                        {
-                            if (c == 'c') { tab[i, j] = '.'; }
-                            else if (c == '0') { tab[i, j] = '0'; }
-                            else if (c == '1') { tab[i, j] = '1'; }
-                        }
-                    }
-                }
-
-                // escape
-                if (c == 'q') return;
-            }
-
-
-            static bool TabLleno(char[,] tab)
-            {
-                for (int i = 0; i < N; i++)
-                {
-                    for (int j = 0; j < N; j++)
-                    {
-                        if (tab[i, j] == '.') { return false; }
-                    }
-                }
-                return true;
-            }
-
-            static void SacaFilCol(int k, char[,] tab, char[] filk, char[] colk)
-            {
-                for (int i = 0; i < N; i++)
-                {
-                    filk[i] = tab[i, k];
-                    for (int j = 0; j < N; j++)
-                    {
-                        colk[j] = tab[i, j];
-                    }
-                }
-
-                //debug
-                for (int i = 0; i < N; i++)
-                {
-                    Console.Write(colk[i] + " ");
+                    if (tab[i, j] == '.') { fijas[i, j] = false; }
+                    else { fijas[i, j] = true; }
                 }
             }
+            fil = col = 0;
+            Console.SetCursorPosition(col, fil);
+        }
 
-            bool TresSeguidos(char[] v)
+        static void Renderiza(char[,] tab, bool[,] fijas, int fil, int col)
+        {
+            Console.Clear();
+            for (int i = 0; i < N; i++)
             {
-                int ceroCount = 0;
-                int unoCount = 0;
-
-                for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
                 {
-                    if (v[i] == '0') { ceroCount++; }
-                    else if (v[i] == '1') { unoCount++; }
-                    else
+                    // Color del texto
+                    if (fijas[i, j] == true) { Console.ForegroundColor = ConsoleColor.Blue; }
+                    else { Console.ForegroundColor = ConsoleColor.Yellow; }
+
+                    // Color del fondo dependiendo de la posición del jugador
+                    if (col == j && fil == i)
                     {
-                        ceroCount = 0;
-                        unoCount = 0;
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write(tab[i, j]);
+                        Console.ResetColor();
+                        Console.Write(" ");
                     }
+                    else { Console.Write(tab[i, j] + " "); }
+                }
+                Console.WriteLine();
+            }
+            Console.ResetColor();
+        }
+
+        static char LeeInput()
+        {
+            char d = ' ';
+            string tecla = Console.ReadKey(true).Key.ToString();
+
+            switch (tecla)
+            {
+                // Movimiento del cursor 	
+                case "LeftArrow": d = 'l'; break;
+                case "UpArrow": d = 'u'; break;
+                case "RightArrow": d = 'r'; break;
+                case "DownArrow": d = 'd'; break;
+
+                // Casillas
+                case "Spacebar": d = 'c'; break;
+                case "D0": d = '0'; break;
+                case "D1": d = '1'; break;
+
+                // Terminar juego
+                case "Escape": case "q": d = 'q'; break;
+
+                default: d = ' '; break;
+            }
+            return d;
+        }
+
+        static void ProcesaInput(char c, char[,] tab, bool[,] fijas, ref int fil, ref int col)
+        {
+            // eje x
+            if (c == 'l' && col > 0) { col--; }
+            else if (c == 'r' && col < N - 1) { col++; }
+            // eje y
+            if (c == 'd' && fil < N - 1) { fil++; }
+            else if (c == 'u' && fil > 0) { fil--; }
+
+            // casillas
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (fijas[i, j] == false && col == j && fil == i)
+                    {
+                        if (c == 'c') { tab[i, j] = '.'; }
+                        else if (c == '0') { tab[i, j] = '0'; }
+                        else if (c == '1') { tab[i, j] = '1'; }
+                    }
+                }
+            }
+
+            // escape
+            if (c == 'q') return;
+        }
+
+
+        static bool TabLleno(char[,] tab)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (tab[i, j] == '.') { return false; }
+                }
+            }
+            return true;
+        }
+
+        static void SacaFilCol(int k, char[,] tab, char[] filk, char[] colk)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                colk[i] = tab[i, k];
+                filk[i] = tab[k, i];
+            }
+        }
+
+        static bool TresSeguidos(char[] v)
+        {
+            int ceroCount = 0;
+            int unoCount = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (v[i] == '0')
+                {
+                    ceroCount++;
+                    unoCount = 0;
+                }
+                else if (v[i] == '1')
+                {
+                    unoCount++;
+                    ceroCount = 0;
+                }
+                else
+                {
+                    ceroCount = 0;
+                    unoCount = 0;
                 }
 
                 if (ceroCount == 3 || unoCount == 3) { return true; }
-                return false;
+                // CONDICIÓN DE CONTADOR DENTRO DEL FOR (recordatorio xq soy un inútil)
             }
-
-            bool IgCerosUnos(char[] v)
-            {
-                int ceroCount = 0;
-                int unoCount = 0;
-
-                for (int i = 0; i < N; i++)
-                {
-                    if (v[i] == '0') { ceroCount++; }
-                    else if (v[i] == '1') { unoCount++; }
-                }
-                if (ceroCount == unoCount) {  return true; }
-                return false;
-            }
-
-            static void MuestraResultado(char[,] tab)
-            {
-                
-            }
-
-            //end
+            // SI LO PUSIESE AQUÍ, no sirve para nada, se queda con las cuentas de la última vuelta de i
+            return false;
         }
+
+        static bool IgCerosUnos(char[] v)
+        {
+            int ceroCount = 0;
+            int unoCount = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (v[i] == '0') { ceroCount++; }
+                else if (v[i] == '1') { unoCount++; }
+            }
+            if (ceroCount == unoCount) { return true; }
+            return false;
+        }
+
+        static bool MuestraResultado(char[,] tab)
+        {
+            char[] colk = new char[N];
+            char[] filk = new char[N];
+
+            bool fallo = false;
+
+            for (int i = 0; i < N; i++)
+            {
+                SacaFilCol(i, tab, filk, colk); // método DENTRO del for
+
+                // tres seguidos
+                if (TresSeguidos(filk))
+                {
+                    Console.WriteLine("Tres iguales seguidos en la fila " + i);
+                    fallo = true;
+                }
+                if (TresSeguidos(colk))
+                {
+                    Console.WriteLine("Tres iguales seguidos en la columna " + i);
+                    fallo = true;
+                }
+
+                // ceros y unos
+                if (!IgCerosUnos(filk))
+                {
+                    Console.WriteLine("No mismo número de 0s y 1s en la fil. " + i);
+                    fallo = true;
+                }
+                if (!IgCerosUnos(colk))
+                {
+                    Console.WriteLine("No mismo número de 0s y 1s en la col. " + i);
+                    fallo = true;
+                }
+
+                Console.WriteLine();
+            }
+            return fallo;
+        }
+
+        //end
     }
 }
