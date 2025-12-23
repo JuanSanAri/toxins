b073b54364c12e5fb8de954b05c294290b31e696
Juan <juasan15@ucm.es>
2025-12-23 18:09:17 +0100

    Empezamos extra 2023
    nos la quitamos entera hoy que quiero empezar mozos

---
+++ Progra1/exams/Extra_2023/Extraordinaria2023/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+class Hitori
+{
+    static void Main()
+    {
+        int[,] tab;      // números del tablero
+        bool[,] tachadas; // casillas tachadas
+        int fil, col;     // posición del cursor
+
+        fil = col = 0;
+
+        tab = new int[,]{
+            {4, 4, 1, 2},
+            {3, 2, 3, 1},
+            {1, 3, 2, 4},
+            {2, 1, 4, 3}};
+
+        tachadas = new bool[,]{
+            {false, false, false, false},
+            {false, false, false, false},
+            {false, false, false, false},
+            {false, false, false, false}};
+
+
+    } // Main
+
+
+
+    static char LeeInput()
+    {
+        char d = ' ';
+
+        string tecla = Console.ReadKey(true).Key.ToString();
+        switch (tecla)
+        {
+            /* INPUTS ELEMENTALES PARA EL JUEGO BÁSICO */
+
+            // movimiento del cursor 	
+            case "LeftArrow": d = 'l'; break;
+            case "UpArrow": d = 'u'; break;
+            case "RightArrow": d = 'r'; break;
+            case "DownArrow": d = 'd'; break;
+
+            // pulsacion de casilla (click)
+            case "Spacebar": d = 'c'; break;
+
+            // terminar juego
+            case "Escape": case "q": d = 'q'; break;
+            default: d = ' '; break;
+        }
+        return d;
+    }
+
+
+}
