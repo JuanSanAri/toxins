9e9d2dd8cd1f1a186a0dafbddaca32d903ce41cb
Juan <juasan15@ucm.es>
2026-06-29 06:17:15 +0200

    update fp1

---
+++ Primero/FP1/exams/Extra2024_fp1/Extra2024_fp1/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace letrasDeslizantes{
+    class Program{
+        static Random rnd = new Random(); // generador de números aleatorios
+
+        struct Coor { // Coordenadas para representar posiciones
+            public int fil, col; }
+
+        static void Main() {
+            char[,] tab; // tablero de juego
+            Coor pos; // posicion actual del cursor
+
+
+        }
+
+
+
+
+        static char LeeInput()
+        {
+            char d = ' ';
+            if (Console.KeyAvailable)
+            {
+                string tecla = Console.ReadKey(true).Key.ToString();
+                switch (tecla)
+                {
+                    /* INPUTS ELEMENTALES PARA EL JUEGO BÁSICO */
+                    // movimiento del cursor 	
+                    case "LeftArrow": d = 'l'; break;
+                    case "UpArrow": d = 'u'; break;
+                    case "RightArrow": d = 'r'; break;
+                    case "DownArrow": d = 'd'; break;
+                    case "Spacebar": d = 's'; break;
+
+                    // terminar juego
+                    case "Escape": case "q": case "Q": d = 'q'; break;
+
+                    default: d = ' '; break;
+                }
+            }
+
+            return d;
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
