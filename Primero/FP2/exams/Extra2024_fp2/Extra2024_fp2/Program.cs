ece5bbc613dd447571c6d37af6f4482e0698d743
Juan <juasan15@ucm.es>
2026-07-01 14:48:16 +0200

    extra24fp2 y chlog
    actualizado changelog y hecho mitad del examen de extra24

--- Primero/FP2/exams/Extra2024_fp2/Extra2024_fp2/Program.cs
+++ Primero/FP2/exams/Extra2024_fp2/Extra2024_fp2/Program.cs
@@ -1,50 +1,74 @@
 
 using System;
 
-namespace LightsOut{
-    class MainClass  {
-        public static void Main(){
-  			string [] lineas = {
-			    "***..", // fila 0
+namespace LightsOut
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            string[] lineas = {
+                "***..", // fila 0
 			    "*..*.", // fila 1
 			    "..***", // ...
 			    "*.*..",
-			    "..*.." };
-			Tablero t = new Tablero(lineas); //llamada a la constructora
+                "..*.." };
 
-			// bucle ppal	
-            
+
+            Tablero t = new Tablero(lineas); //llamada a la constructora
+            // Variables y cosas
+            Console.CursorVisible = false;
+            bool terminado = false;
+            bool win = false;
+            char c;
+            while (!terminado && !win)
+            {
+                c = LeeInput();
+                if (c == 'q') terminado = true;
+                else
+                {
+                    t.ProcesaInput(c);
+                    t.Dibuja();
+                    t.FinJuego(out win);
+                }
+            }
+            Console.Clear();
+            if (win) Console.WriteLine("Has ganado!!");
+            else Console.WriteLine("Has salido con ESC.");
         }
 
-        static char LeeInput(){
-		    char d = ' ';			
-			if (Console.KeyAvailable) {	
-				string tecla = Console.ReadKey (true).Key.ToString ();
-				switch (tecla) {
-                // movimiento del cursor 	
-				case "LeftArrow":  d = 'l'; break;
-				case "UpArrow":    d = 'u'; break;
-				case "RightArrow": d = 'r'; break;
-				case "DownArrow":  d = 'd'; break;				
-				// pulsacion de casilla (click)
-				case "Spacebar": d = 'c'; break;				
-				// terminar juego
-				case "Escape": case "q": d = 'q'; break;
-				// deshacer jugada
-				case "z": case "Z": d='z'; break;
-				// guardar partida
-				case "s": case "S": d='s'; break;
-				// recuperar partida guardada
-				case "o": case "O":	d='o'; break;				
-                // reiniciar estado original del juego
-				case "i": case "I": d='i'; break;
-				default: d=' '; break;
-				}
-			}  			
-			return d;
-                     
-		}
+        static char LeeInput()
+        {
+            char d = ' ';
+            //if (Console.KeyAvailable)
+            {
+                string tecla = Console.ReadKey(true).Key.ToString();
+                switch (tecla)
+                {
+                    // movimiento del cursor 	
+                    case "LeftArrow": d = 'l'; break;
+                    case "UpArrow": d = 'u'; break;
+                    case "RightArrow": d = 'r'; break;
+                    case "DownArrow": d = 'd'; break;
+                    // pulsacion de casilla (click)
+                    case "Spacebar": d = 'c'; break;
+                    // terminar juego
+                    case "Escape": case "q": d = 'q'; break;
+                    // deshacer jugada
+                    case "z": case "Z": d = 'z'; break;
+                    // guardar partida
+                    case "s": case "S": d = 's'; break;
+                    // recuperar partida guardada
+                    case "o": case "O": d = 'o'; break;
+                    // reiniciar estado original del juego
+                    case "i": case "I": d = 'i'; break;
+                    default: d = ' '; break;
+                }
+            }
+            return d;
 
-    }   
+        }
+
+    }
 
 }
--- Primero/FP2/exams/Extra2024_fp2/Extra2024_fp2/Tablero.cs
+++ Primero/FP2/exams/Extra2024_fp2/Extra2024_fp2/Tablero.cs
@@ -2,19 +2,96 @@ using System;
 using System.IO;
 using Listas;
 
-namespace LightsOut{
-    class Tablero{
+namespace LightsOut
+{
+    class Tablero
+    {
         int FILS, COLS;     // numero de filas y columnas del tablero       
-        bool [,] mat;       // matriz de casillas del tablero
+        bool[,] mat;       // matriz de casillas del tablero
         struct Posicion { public int fil, col; }    // tipo para coordenadas 2D    
         Posicion cursor; // coordenadas de la posicion del cursor
-        
+
         // lista de posiciones para guardar jugadas y poder deshacerlas
         // Lista MemoJugadas; 
 
-        // metodos pedidos
 
+        public Tablero(string[] lineas)
+        {
+            // FILS, COLS
+            FILS = lineas.Length;
+            COLS = lineas[0].Length;
+            // mat
+            mat = new bool[FILS, COLS];
+            for (int i = 0; i < FILS; i++)
+            {
+                for (int j = 0; j < COLS; j++)
+                {
+                    if (lineas[i][j] == '*') mat[i, j] = true;
+                    else mat[i, j] = false;
+                }
+            }
+            // Cursor
+            cursor.fil = cursor.col = 0;
+        }
 
+        public void Dibuja()
+        {
+            Console.Clear();
+            // Tablero
+            for (int i = 0; i < FILS; i++)
+            {
+                for (int j = 0; j < COLS; j++)
+                {
+                    if (mat[i, j]) Console.BackgroundColor = ConsoleColor.Yellow;
+                    else Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.Write("  ");
+                }
+                Console.WriteLine();
+            }
+            // Cursor
+            Console.SetCursorPosition(2 * cursor.col, cursor.fil);
+            if (mat[cursor.fil, cursor.col]) Console.BackgroundColor = ConsoleColor.Yellow;
+            else Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("<>");
+            Console.ResetColor();
+        }
 
+        private void ClickCasilla()
+        {
+            mat[cursor.fil, cursor.col] = !mat[cursor.fil, cursor.col];
+            if (cursor.fil - 1 >= 0) mat[cursor.fil - 1, cursor.col] = !mat[cursor.fil - 1, cursor.col];
+            if (cursor.fil + 1 <= FILS - 1) mat[cursor.fil + 1, cursor.col] = !mat[cursor.fil + 1, cursor.col];
+            if (cursor.col - 1 >= 0) mat[cursor.fil, cursor.col - 1] = !mat[cursor.fil, cursor.col - 1];
+            if (cursor.col + 1 <= COLS - 1) mat[cursor.fil, cursor.col + 1] = !mat[cursor.fil, cursor.col + 1];
+        }
+
+        public void FinJuego(out bool fin)
+        {
+            fin = true;
+            int i = 0;
+            int j = 0;
+            while (i < FILS && fin)
+            {
+                while (j < COLS && fin)
+                {
+                    if (mat[i, j]) fin = false;
+                    j++;
+                }
+                j = 0;
+                i++;
+            }
+        }
+
+        public void ProcesaInput(char d)
+        {
+            switch (d)
+            {
+                case 'u': if (cursor.fil > 0) cursor.fil--; break;
+                case 'd': if (cursor.fil < FILS - 1) cursor.fil++; break;
+                case 'l': if (cursor.col > 0) cursor.col--; break;
+                case 'r': if (cursor.col < COLS - 1) cursor.col++; break;
+            }
+            if (d == 'c') ClickCasilla();
+        }
     }
 }
